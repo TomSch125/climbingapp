e9e1d877f23e9224a463ca8533eed09f79284022
Tom Schofield <ed19ts3@leeds.ac.uk>
2024-04-27 13:38:17 +0100

    sprigboot started

---
+++ ClimbingApp/ClimbingApp/src/main/java/com/example/ClimbingApp/ClimbingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClimbingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbingAppApplication.class, args);
+	}
+
+}
---
+++ ClimbingApp/ClimbingApp/src/test/java/com/example/ClimbingApp/ClimbingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClimbingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
